c9c1754a81a013ef8e89f86a61523a35a713a489
DESKTOP-S1M18AA\khiem <nguyenkhiem7789@gmail.com>
2021-07-22 11:59:55 +0700

    create project

---
+++ LTE-ASP-Base/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LTE_ASP_Base.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
